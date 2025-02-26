679f6673b0134fcf3921cf075f2ee505bb19ac6b
Your Name <your-email@example.com>
2025-02-25 23:10:35 -0800

    first

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ const heading = document.querySelector("#text");
 const btn = document.querySelector("#btn");
 const color = document.getElementById("color");
 const parent = document.querySelector("#parent");
+const undo = document.querySelector("#undo");
 //const span =document.querySelector("span");
-
+let deletnot=[]
 btn.addEventListener("click", () => {
     const not = heading.value.trim();
     const textcolor = color.value;
     heading.classList.add("book")
-
-   const div = document.createElement("div");
-    div.style.backgroundColor = textcolor;
-    div.textContent = not;
-
+    if( not=== ""){
+        alert("plese enter text")
+    }
 
     const currentDate = new Date().toLocaleString();
     // const date = currentDate.toLocaleString();
-    // const time =currenTime.toLocaleString()
+    // const time =currenTime.toLocaleString();
+
+    const div = document.createElement("div");
+    div.style.backgroundColor = textcolor;
+    // div.textContent = not;
+
 
     const para=document.createElement("p")
-    // para.textContent=not;
+    para.textContent=not;
     div.append(para);
 
 
@@ -33,18 +37,30 @@ btn.addEventListener("click", () => {
     span.classList.add("span");
     span.innerHTML="&times";
 
-    span.addEventListener("click" , (e)=>{
-        e.target.parentElement.remove()
+
+    span.addEventListener("click",(e)=>{
+        const deletedHote = e.target.parentElement;
+        deletnot.push(deletedHote);
+        deletedHote.remove()
      })
 
     
-
      div.append(span);
     parent.append(div);
    
     heading.value=""
 });
-
+undo.addEventListener("click",()=>{
+    if(deletnot.length>0){
+        const  lastdelet=deletnot.pop();
+        parent.appendChild(lastdelet);
+    }
+    else{
+        alert("plese remove text")
+    }
+    console.log();
+    
+})
 
 
 // const currentDate = new Date();
